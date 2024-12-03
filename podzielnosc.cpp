e65bc46d334fee13d4397575fa997f4f14c605b2
Filip Mateusz Zuchnicki <filzu@lts.mat.umk.pl>
2024-12-03 12:38:02 +0100

    Dodanie wypisywania gcd

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+int nwd(int a, int b){
+    int liczba = min(a,b);
+    while(liczba>1){
+        if(a%liczba==0 && b%liczba==0){
+	    break;
+	}
+      liczba--;
+    }
+    return liczba;
+}
 
 int main(){
     int a,b;
     cin >> a >> b;
     cout << a << ',' << b << endl;
+    cout << "gcd: " << nwd(a,b) << endl;
     return 0;
 }
