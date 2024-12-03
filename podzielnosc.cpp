286305229052873fac013bc165c6f898d84476ad
Filip Mateusz Zuchnicki <filzu@lts.mat.umk.pl>
2024-12-03 12:50:59 +0100

    Dodanie sprawdzenia czy wpisane liczby sa dodatnie

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
     int a,b;
     cin >> a >> b;
+    if(a<0 || b < 0){
+        cout << "Wartosci nie sa dodatnie" << endl;
+	return -1;
+    }
     cout << a << ',' << b << endl;
     return 0;
 }
